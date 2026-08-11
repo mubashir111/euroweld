92e07bfd0fcf4cebb36df310ae40bd7c04db3d09
mubashir111 <muba4shir@gmail.com>
2026-08-11 15:58:56 +0530

    added new changes

--- fix_animation.py
+++
@@ -1,9 +0,0 @@
-with open('index.html', 'r') as f:
-    html = f.read()
-
-# Remove 'opacity: 1 !important;' from the inline styles
-html = html.replace('opacity: 1 !important; ', '')
-
-with open('index.html', 'w') as f:
-    f.write(html)
-print("Opacity override removed.")
